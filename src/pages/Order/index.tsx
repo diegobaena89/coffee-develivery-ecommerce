5bd98b121973085345ee5052b5dac7a6b020d64f
diegobaena89 <diegobf.cello@gmail.com>
2023-01-14 12:14:11 -0300

    feat - create Address form and payment choices in order component

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,9 +1,73 @@
-import { OrderContainer } from './styles';
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react';
+import {
+  AddressContainer,
+  AddressFrame,
+  ChoosePayment,
+  FinishOrder,
+  FormContainer,
+  Forms,
+  OrderContainer,
+  PaymentFrame,
+  PaymentMethod,
+} from './styles';
 
 export function Order() {
   return (
     <OrderContainer>
-      <p>teste</p>
+      <FormContainer>
+        <h2>Complete seu pedido</h2>
+        <AddressContainer>
+          <AddressFrame>
+            <MapPinLine size={22} />
+            <div>
+              <p>Endereço de Entrega</p>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </AddressFrame>
+          <Forms>
+            <input type='text' placeholder='CEP' />
+            <input type='text' placeholder='Rua' />
+            <input type='number' name='Numero' placeholder='Número' />
+            <input type='text' placeholder='Complemento' />
+            <input type='text' placeholder='Bairro' />
+            <input type='text' placeholder='Cidade' />
+            <input type='text' placeholder='UF' />
+          </Forms>
+        </AddressContainer>
+
+        <PaymentMethod>
+          <PaymentFrame>
+            <CurrencyDollar size={22} weight='fill' />
+            <div>
+              <p>Pagamento</p>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </PaymentFrame>
+          <ChoosePayment>
+            <button>
+              <CreditCard size={16} />
+              Cartão de Crédito
+            </button>
+            <button>
+              <Bank size={16} /> Cartão de Débito
+            </button>
+            <button>
+              <Money size={16} /> Dinheiro
+            </button>
+          </ChoosePayment>
+        </PaymentMethod>
+      </FormContainer>
+      <FinishOrder>
+        <h2>Cafés selecionados</h2>
+      </FinishOrder>
     </OrderContainer>
   );
 }
--- src/pages/Order/styles.ts
+++ src/pages/Order/styles.ts
@@ -1,8 +1,164 @@
 import styled from 'styled-components';
 
-export const OrderContainer = styled.div`
+export const OrderContainer = styled.form`
   width: 1140px;
   margin: 40px auto;
   height: auto;
-  border: 1px solid black;
+  display: flex;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FormContainer = styled.div`
+  width: 640px;
+  height: auto;
+
+  > h2 {
+    font-family: 'Baloo 2';
+    font-size: 18px;
+    color: var(--base-subtitle);
+  }
+`;
+
+export const AddressContainer = styled.div`
+  width: 100%;
+  height: 372px;
+  background: var(--base-card);
+  margin: 20px 0 12px;
+  border-radius: 6px;
+  padding: 40px 0;
+`;
+
+export const AddressFrame = styled.div`
+  display: flex;
+  width: 560px;
+  margin: 0 auto;
+  margin-bottom: 32px;
+
+  & p {
+    font-size: 1rem;
+    color: var(--base-subtitle);
+  }
+
+  & span {
+    font-size: 14px;
+    color: var(--base-text);
+  }
+
+  svg {
+    color: var(--yellow-dark);
+    margin-right: 8px;
+  }
+`;
+
+export const Forms = styled.div`
+  width: 560px;
+  margin: 0 auto;
+
+  input {
+    height: 42px;
+    background-color: var(--base-input);
+    border: 1px solid var(--base-button);
+    border-radius: 4px;
+    padding: 12px;
+    font-size: 14px;
+    margin-bottom: 16px;
+  }
+
+  & input:nth-child(1) {
+    display: block;
+    width: 200px;
+  }
+
+  & input:nth-child(2) {
+    width: 100%;
+  }
+
+  & input:nth-child(3) {
+    width: 200px;
+    margin-right: 10px;
+  }
+
+  & input:nth-child(4) {
+    width: 348px;
+  }
+
+  & input:nth-child(5) {
+    width: 200px;
+    margin-right: 10px;
+  }
+
+  & input:nth-child(6) {
+    width: 276px;
+    margin-right: 12px;
+  }
+
+  & input:nth-child(7) {
+    width: 60px;
+  }
+`;
+
+export const PaymentMethod = styled.div`
+  width: 100%;
+  height: 207px;
+  background: var(--base-card);
+  margin: 20px 0 0px;
+  border-radius: 6px;
+  padding: 40px 0;
+`;
+
+export const PaymentFrame = styled.div`
+  display: flex;
+  width: 560px;
+  margin: 0 auto;
+  margin-bottom: 32px;
+
+  & p {
+    font-size: 1rem;
+    color: var(--base-subtitle);
+  }
+
+  & span {
+    font-size: 14px;
+    color: var(--base-text);
+  }
+
+  svg {
+    color: var(--brand-purple-md);
+    margin-right: 8px;
+  }
+`;
+
+export const ChoosePayment = styled.div`
+  width: 560px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+    width: 178px;
+    height: 51px;
+    color: var(--base-text);
+    text-transform: uppercase;
+    font-size: 12px;
+    border-radius: 6px;
+    border: none;
+    background-color: var(--base-button);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  & svg {
+    margin-right: 12px;
+    color: var(--brand-purple-md);
+  }
+`;
+
+export const FinishOrder = styled.div`
+  width: 448px;
+  height: 498px;
+  border: 1px solid red;
 `;
